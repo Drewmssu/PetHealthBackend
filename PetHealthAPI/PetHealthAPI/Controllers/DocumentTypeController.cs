0a64400cff3a1b2d5ff8d880e99ec07ceb86063e
DESKTOP-HB0C1U8\Usuario <gonzalo_cirilo@hotmail.com>
2017-11-13 10:55:55 -0500

    Document type controller added

---
+++ PetHealthAPI/PetHealthAPI/Controllers/DocumentTypeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetHealthAPI.Controllers
+{
+    public class DocumentTypeController : BaseController
+    {
+        // GET: DocumentType
+        public JsonResult DocumentType()
+        {
+            var lst = context.DocumentType.Where(x=>x.Status=="ACT").Select(x => new
+            {
+                id = x.DocumentTypeId,
+                abreviation = x.Abreviation,
+                name = x.Name
+            }).ToList();
+            return Json(new {status="ok", documents=lst }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
